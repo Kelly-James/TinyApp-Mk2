f126cfc47536f6473e04b9b1509ae00f6853ac4c
Kelly James <kellyjamesclancy@gmail.com>
2016-11-09 01:24:12 +0000

    Initial commit; app functionaing; includes views;

---
+++ express_server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+
+var urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca' ,
+  '9sm5xK': 'http://www.google.com'
+};
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/urls', (req, res) => {
+  res.render('pages/urls_index', {urls: urlDatabase});
+});
+
+app.get('/urls/:id', (req, res) => {
+  res.render('pages/urls_show', {shortURL: req.params.id, longURL: urlDatabase[req.params.id]});
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.end('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
