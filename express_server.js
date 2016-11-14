3a067942482660f9aa36879ca45e9294086f49f7
Kelly James <kellyjamesclancy@gmail.com>
2016-11-14 21:32:20 +0000

    Fixed bugs throughout;

--- express_server.js
+++ express_server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-const password = 'purple-monkey-dinosaur';
-const hashed_password = bcrypt.hashSync(password, 10);
 const stringGen = require('./lib/string_gen.js');
 const longUrlFinder = require('./lib/url_finder.js');
 const bodyParser = require('body-parser');
@@ -23,13 +21,26 @@ app.use(function(req, res, next) {
     let currentUser = userDatabase[req.session.userId];
     req.currentUser = currentUser;
     res.locals.currentUser = currentUser;
+    // console.log(userDatabase[req.session.userId]);
   } else {
     res.locals.currentUser = null;
   }
   next();
 });
 
-const userDatabase = {};
+const userDatabase = {
+      'a12345': {
+            id: 'a12345',
+            username: 'user1',
+            email: 'user1@users.com',
+            password: 'encrypted',
+            urls: {}
+      }
+};
+const urlDatabase = {
+      'b2xVn2': 'http://www.lighthouselabs.ca' ,
+      '9sm5xK': 'http://www.google.com'
+  };
 
 app.get('/', (req, res) => {
   res.render('pages/index');
@@ -52,22 +63,26 @@ app.post('/register', (req, res) => {
                           urls: {}
                         };
   req.session.userId = userId;
-  console.log(userDatabase[userId]);
-  console.log(req.session.userId);
   res.redirect('/');
 })
 
 // request for urls_index view;
-app.get('/urls', (req, res) => {
+app.get('/your_urls', (req, res) => {
   if(req.session.userId) {
-    res.render('pages/urls_index');
+    res.render('pages/user_urls_index');
   } else {
-    res.redirect('/');
+    res.redirect('/urls');
   }
 });
 
+app.get('/urls', (req, res) => {
+  let urls = urlDatabase;
+  res.render('pages/noUser_urls_index', {urls: urlDatabase});
+});
+
 // makes post to urls_index; invokes random string generator module, sets shortURL to return value; longURL set to request body.longURL; if statement checks for presence of 'http' prefix; adds new key/value pair to database object
-app.post('/urls', (req, res) => {
+app.post('/your_urls', (req, res) => {
+  // console.log(userDatabase);
   if(req.session.userId) {
     let shortURL = stringGen.generator();
     let longURL = req.body.longURL;
@@ -75,7 +90,8 @@ app.post('/urls', (req, res) => {
       longURL = 'http://' + longURL;
     }
     res.locals.currentUser['urls'][shortURL] = longURL;
-    res.redirect('/urls');
+    urlDatabase[shortURL] = longURL;
+    res.redirect('/your_urls');
   } else {
     res.redirect('/');
   }
@@ -91,8 +107,9 @@ app.get('/new', (req, res) => {
 
 // makes get request to the value of longURL for original page
 app.get('/u/:shortURL', (req, res) => {
+  console.log(userDatabase);
   let shortURL = req.params.shortURL;
-  let longURL = longUrlFinder.find(shortURL);
+  let longURL = longUrlFinder.finder(shortURL, userDatabase);
   if(longURL) {
     res.redirect(longURL);
   } else {
@@ -121,18 +138,17 @@ app.get('/forbidden', (req, res) => {
 });
 
 // makes post request to urls_index to delete a url; redirects to urls_index
-app.post('/urls/:id/delete', (req, res) => {
+app.post('/your_urls/:id/delete', (req, res) => {
   if(req.session.userId) {
     delete res.locals.currentUser['urls'][req.params.id];
-    res.redirect('/urls');
+    res.redirect('/your_urls');
   } else {
     res.redirect('/');
   }
 });
 
 // makes post request to urls_index to update the longURL; redirects to urls_index
-app.post('/urls/:id/update', (req, res) => {
-  debugger;
+app.post('/your_urls/:id/update', (req, res) => {
   if(req.session.userId) {
     let shortURL = req.params.id;
     let longURL = req.body.longURL;
@@ -140,29 +156,31 @@ app.post('/urls/:id/update', (req, res) => {
       longURL = 'http://' + longURL;
     }
     res.locals.currentUser['urls'][shortURL] = longURL;
-    res.redirect('/urls');
+    res.redirect('/your_urls');
   } else {
     res.redirect('/');
   }
 });
 
 app.post('/login', (req, res) => {
-  let foundUser = Object.keys(userDatabase)
-                        .find(function(userId, i, userNames) {
-    return userDatabase[userId].username === req.body.username;
-  });
-  let passMatch = bcrypt.compareSync(req.body.password, userDatabase[foundUser].password);
-  if(foundUser && passMatch) {
-    req.session.userId = foundUser;
-    console.log(req.session.userId);
-    res.redirect('/');
-  } else {
-    res.status(403).redirect('/forbidden');
-  }
+    let foundUser = Object.keys(userDatabase)
+      .find(function(userId) {
+        return userDatabase[userId].username === req.body.username;
+      });
+    let passMatch = false;
+    if(foundUser) {
+       passMatch = bcrypt.compareSync(req.body.password, userDatabase[foundUser].password);
+    };
+    if(foundUser && passMatch) {
+      req.session.userId = foundUser;
+      res.redirect('/');
+    } else {
+      res.status(403).redirect('/forbidden');
+    }
 });
 
 app.post('/logout', (req, res) => {
-  req.session = null;
+  req.session.userId = null;
   res.redirect('/');
 });
 
